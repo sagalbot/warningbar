bf5068d8fdf96cf10b921b23011ab621d660abc3
Jeff <sagalbot@Jeffs-MacBook-Pro.local>
2017-04-04 10:48:07 -0700

    update manifest, webpack, improve buttons

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,17 @@ var options = {
   module: {
     rules: [
       { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
-      { test: /\.css$/, loader: "style-loader!css-loader", exclude: /node_modules/  }
+      { test: /\.css$/, loader: "style-loader!css-loader", exclude: /node_modules/  },
+      {
+          test: /\.scss$/,
+          use: [{
+              loader: "style-loader" // creates style nodes from JS strings
+          }, {
+              loader: "css-loader" // translates CSS into CommonJS
+          }, {
+              loader: "sass-loader" // compiles Sass to CSS
+          }]
+      }
     ]
   },
   resolve: {
